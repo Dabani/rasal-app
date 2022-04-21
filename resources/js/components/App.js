ead549ee4726661bfdc57277caef03b6514b0302
DabaniCode <dabani.rm@gmail.com>
2022-04-21 10:34:04 +0200

    Initialize react Rasal App

---
+++ resources/js/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                {/* Components go here */}
+                <h1>Components</h1>
+            </div>
+        </div>
+    );
+}
+
+export default App;
--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -7,9 +7,9 @@ function Example() {
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Example Component</div>
+                        <div className="card-header">RASAL App Component</div>
 
-                        <div className="card-body">I'm an example component!</div>
+                        <div className="card-body">I'm a RASAL App component!</div>
                     </div>
                 </div>
             </div>
@@ -18,7 +18,3 @@ function Example() {
 }
 
 export default Example;
-
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
-}
